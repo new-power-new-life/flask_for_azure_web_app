21c52f153946922c47182f0ef6c1d09489974960
Rayyyy <chiahung2000@gmail.com>
2022-10-04 23:07:32 +0800

    add app.py homepage

--- app.py
+++ app.py
@@ -12,27 +12,27 @@ app = Flask(__name__)
 
 
 # the minimal Flask application
-@app.route('/')
+@app.route("/")
 def index():
-    return '<h1>Hello, World! 2022-10-04  AAA </h1>'
+    return "<h1>Hello, World! 2022-10-04  AAA ???  Add text for DevOps !!!! for DevOps !!!! for DevOps !!!! for DevOps !!!! for DevOps !!!! for DevOps !!!! </h1>"
 
 
 # bind multiple URL for one view function
-@app.route('/hi')
-@app.route('/hello')
+@app.route("/hi")
+@app.route("/hello")
 def say_hello():
-    return '<h1>Hello, Flask!</h1>'
+    return "<h1>Hello, Flask!</h1>"
 
 
 # dynamic route, URL variable default
-@app.route('/greet', defaults={'name': 'Programmer'})
-@app.route('/greet/<name>')
+@app.route("/greet", defaults={"name": "Programmer"})
+@app.route("/greet/<name>")
 def greet(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return "<h1>Hello, %s!</h1>" % name
 
 
 # custom flask cli command
 @app.cli.command()
 def hello():
     """Just say hello."""
-    click.echo('Hello, Human!')
+    click.echo("Hello, Human!")
